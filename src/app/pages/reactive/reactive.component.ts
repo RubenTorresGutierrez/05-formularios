615fdee082a01fbf62a142538182032194df7600
Óscar Caballero <85021759+ocaballerog1@users.noreply.github.com>
2022-02-02 08:59:59 +0100

    Reactive Primera Parte

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive',
@@ -7,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ReactiveComponent implements OnInit {
-
-  constructor() { }
+  forma!:FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.crearFormulario()
+  }
 
   ngOnInit(): void {
   }
+  crearFormulario()
+  {
+    this.forma = this.formBuilder.group({
+      nombre:['',[Validators.required, Validators.minLength(5)]],
+      apellido:['',[Validators.required, Validators.minLength(5)]],
+      email:['',[Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]]
+    })
+  }
+
+  guardar(){
+    console.log(this.forma);
+  }
+
+  get nombreValido(){
+    let nombre:any = this.forma.get('nombre');
+    return !(nombre.invalid && nombre.touched);
+  }
+
+
 
 }
