bf5029f6583e8cf09e212b72f021741b5655ede2
Rubén Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2022-02-02 10:10:43 +0100

    Se cargan los datos

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, NgForm, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive',
@@ -8,29 +8,67 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   ]
 })
 export class ReactiveComponent implements OnInit {
+
+  // Atributtes
   forma!:FormGroup;
+
   constructor(private formBuilder: FormBuilder) {
+
     this.crearFormulario()
+    this.cargarDatosFormulario()
+
   }
 
   ngOnInit(): void {
   }
-  crearFormulario()
-  {
+
+  crearFormulario() {
+
     this.forma = this.formBuilder.group({
       nombre:['',[Validators.required, Validators.minLength(5)]],
       apellido:['',[Validators.required, Validators.minLength(5)]],
-      email:['',[Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]]
+      email:['',[Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]],
+      direccion: this.formBuilder.group({
+        distrito:['',Validators.required],
+        ciudad:['',Validators.required]
+      })
     })
+
   }
 
-  guardar(){
-    console.log(this.forma);
+  guardar(forma: FormGroup){
+
+    if (forma.invalid) {
+      Object.values(forma.controls).forEach(control => {
+        if (control instanceof FormGroup)
+          this.guardar(control);
+        control.markAsTouched();
+      })
+      return;
+    }
+
   }
 
-  get nombreValido(){
-    let nombre:any = this.forma.get('nombre');
-    return !(nombre.invalid && nombre.touched);
+  cargarDatosFormulario()  {
+
+    this.forma.setValue({
+      nombre: "kjhlk",
+      apellido: "lkjlkj",
+      email: "ñlkñl",
+      direccion: {
+        distrito: "ñlkñl",
+        ciudad: "ñlkñkl"
+      }
+    })
+
+  }
+
+
+  // GETTERS
+
+  validar(campo1: string){
+    let campo: any = this.forma.get(campo1);
+    return !(campo.invalid && campo.touched);
   }
 
 
